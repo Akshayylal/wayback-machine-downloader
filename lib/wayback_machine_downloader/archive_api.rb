17bb3ae040d4035c8f624497a34b2782981ec9be
Akshayylal <64681214+Akshayylal@users.noreply.github.com>
2023-12-19 12:20:45 +0530

    Update archive_api.rb

--- lib/wayback_machine_downloader/archive_api.rb
+++ lib/wayback_machine_downloader/archive_api.rb
@@ -3,22 +3,30 @@ require 'uri'
 
 module ArchiveAPI
 
-  def get_raw_list_from_api url, page_index
-    request_url = URI("https://web.archive.org/cdx/search/xd")
-    params = [["output", "json"], ["url", url]]
-    params += parameters_for_api page_index
-    request_url.query = URI.encode_www_form(params)
+def get_raw_list_from_api(url, page_index)
+  # Your ScraperAPI key
+  scraperapi_key = 'YOUR_SCRAPERAPI_KEY'
 
-    begin
-      json = JSON.parse(URI(request_url).open.read)
-      if (json[0] <=> ["timestamp","original"]) == 0
-        json.shift
-      end
-      json
-    rescue JSON::ParserError
-      []
+  # Original request setup for Wayback Machine
+  original_request_url = URI("https://web.archive.org/cdx/search/xd")
+  params = [["output", "json"], ["url", url]]
+  params += parameters_for_api(page_index)
+  original_request_url.query = URI.encode_www_form(params)
+
+  # Modify the request to go through ScraperAPI
+  scraperapi_url = URI("http://api.scraperapi.com?api_key=#{scraperapi_key}&url=#{URI.encode(original_request_url.to_s)}")
+
+  begin
+    json = JSON.parse(URI(scraperapi_url).open.read)
+    if (json[0] <=> ["timestamp","original"]) == 0
+      json.shift
     end
+    json
+  rescue JSON::ParserError
+    []
   end
+end
+
 
   def parameters_for_api page_index
     parameters = [["fl", "timestamp,original"], ["collapse", "digest"], ["gzip", "false"]]
